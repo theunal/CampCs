e694f0d12b51859a3eac67c03b11e48294f5a3ef
theunal <97359852+theunal@users.noreply.github.com>
2022-05-04 12:27:38 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,14 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+       
+       
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            Console.WriteLine(productManager.GetAll().Count);
+        }
+    }
+}
