9639c439b8744c92f983805694bc442593fa1e67
theunal <97359852+theunal@users.noreply.github.com>
2022-05-04 17:51:50 +0300

    create api project, singleton

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService productService;
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public IDataResult<List<Product>> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+
+            return productService.GetAll();
+        }
+    }
+}
