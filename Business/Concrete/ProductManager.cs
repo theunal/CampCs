070aaa2627d0b23543a5db35cde07845e9a7f948
theunal <97359852+theunal@users.noreply.github.com>
2022-05-05 09:23:34 +0300

    productvalidator, validationTool, interceptors

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Const;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,12 +25,10 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
-            productDal.Add(product);
+            
+            ValidationTool.Validate(new ProductValidator(), product);
             return new SuccessResult(Messages.ProductAdded);
+            
         }
 
         public IDataResult<List<Product>> GetAll()
---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            // product kuralları
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün ismi boş geçilemez");
+            RuleFor(p => p.ProductName).MinimumLength(2).WithMessage("Ürün ismi en az 2 karakter olmalıdır");
+            RuleFor(p => p.UnitPrice).NotEmpty().WithMessage("Ürün fiyatı boş geçilemez");
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1).WithMessage("Ürün fiyatı 10'dan büyük olmalıdır");
+            RuleFor(p => p.ProductName).Must(StartWithS).WithMessage("Ürün ismi S ile başlamalıdır");
+        }
+
+        private bool StartWithS(string arg)
+        {
+            return arg.StartsWith("S");
+        }
+    }
+}
